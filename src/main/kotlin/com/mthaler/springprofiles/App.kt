d324cce5ecff336c2672d1a9560aa322d621817b
Michael Thaler <michael.thaler@dlr.de>
2021-06-19 08:43:14 +0200

    Added App class.

---
+++ src/main/kotlin/com/mthaler/springprofiles/App.kt
@@ -0,0 +1,24 @@
+package com.mthaler.springprofiles
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.context.ConfigurableApplicationContext
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
+import org.springframework.core.env.AbstractEnvironment
+
+
+class App
+
+private val log: Logger = LoggerFactory.getLogger(App::class.java)
+
+fun main(args: Array<String>) {
+    System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "dev")
+    val context = AnnotationConfigApplicationContext(AppConfig::class.java)
+
+    val obj = context.getBean("movieDao") as MovieDao
+    log.debug("Result : {}", obj.findByDirector("dummy"))
+    log.debug("Result : {}", obj.findByDirector("dummy"))
+    log.debug("Result : {}", obj.findByDirector("dummy"))
+
+    (context as ConfigurableApplicationContext).close()
+}
